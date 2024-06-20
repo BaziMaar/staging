70ef0e2b36b6432be5a47216f93b6c683a8bd705
Kanishk Pareek <knshkpareek@gmail.com>
2024-06-21 02:03:12 +0530

    changes

---
+++ models/AvaitorModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const transactionSchema = new mongoose.Schema({
+    phone: {
+        type: Number,
+        required: true
+    },
+        time: {
+            type: Number,
+            required: true
+        },
+        amount: {
+            type: Number,
+            required: true
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now
+        }
+}, {
+    timestamps: true // This will add createdAt and updatedAt fields to the schema
+});
+
+const AvaitorEntry = mongoose.model('AvaitorEntry', transactionSchema);
+
+module.exports = AvaitorEntry;
