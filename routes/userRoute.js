86f9041554d45868e78ae27cded1b45de387cb45
Kanishk Pareek <knshkpareek@gmail.com>
2024-06-22 13:52:52 +0530

    changes

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -10,8 +10,8 @@ user_route.post('/login', user_controller.userLogin);
 user_route.post('/update',verifyDeviceId,user_controller.updateProfile)
 user_route.get('/getUser',user_controller.getUser)
 user_route.get('/getUpi',user_controller.getUpi)
-user_route.get('/postUpi',user_controller.postUpi)
-user_route.get('/removeUpi',user_controller.deleteUpi)
+user_route.post('/postUpi',user_controller.postUpi)
+user_route.post('/removeUpi',user_controller.deleteUpi)
 user_route.post('/App',user_controller.updateApp)
 user_route.get('/getVersion',user_controller.getVersion)
 module.exports = user_route;
