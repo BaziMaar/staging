6b36bdff990dec2fb32b002a032d3784b8c890b9
Kanishk Pareek <knshkpareek@gmail.com>
2024-07-11 09:46:11 +0530

    changes

--- controllers/notificationController.js
+++ controllers/notificationController.js
@@ -1,34 +1,36 @@
 const admin = require('../firebase');
-const sendNotification=()=>async (req, res) => {
+
+const sendNotification = async (req, res) => {
     const { token, title, body } = req.body;
-  
+
     if (!token || !title || !body) {
-      return res.status(400).send({
-        success: false,
-        msg: 'Token, title, and body are required'
-      });
+        return res.status(400).send({
+            success: false,
+            msg: 'Token, title, and body are required'
+        });
     }
-  
+
     const messagePayload = {
-      notification: {
-        title: title,
-        body: body
-      }
+        notification: {
+            title: title,
+            body: body
+        }
     };
-  
+
     try {
-      const response = await admin.messaging().sendToDevice(token, messagePayload);
-      return res.status(200).send({
-        success: true,
-        msg: 'Successfully sent message',
-        response
-      });
+        const response = await admin.messaging().sendToDevice(token, messagePayload);
+        return res.status(200).send({
+            success: true,
+            msg: 'Successfully sent message',
+            response
+        });
     } catch (error) {
-      return res.status(500).send({
-        success: false,
-        msg: 'Error sending message',
-        error: error.message
-      });
+        return res.status(500).send({
+            success: false,
+            msg: 'Error sending message',
+            error: error.message
+        });
     }
-  }
-  module.exports= {sendNotification};
+};
+
+module.exports = { sendNotification };
