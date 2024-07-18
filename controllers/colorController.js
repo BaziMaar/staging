6ee42837cf282670226cd5fffbe852781d6316ee
Kanishk Pareek <knshkpareek@gmail.com>
2024-07-18 07:24:42 +0530

    changes

--- controllers/colorController.js
+++ controllers/colorController.js
@@ -101,7 +101,7 @@ function getIndexFromNonZero(arr) {
 
 function checkOnlyPlacedBet(arr, i) {
   for (let j = 0; j < arr.length; j++) {
-    if (arr[j] !== 0 && arr[j] !== arr[i]) {
+    if (arr[j] !== 0 && arr[j] !== arr[i]&&(i!==0||i!==5)) {
       return false; 
     }
   }
