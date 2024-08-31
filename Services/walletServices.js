1fc7fbd309d3e63ba2ce9a0c2a7bb3d8bb52f923
Kanishk Pareek <knshkpareek@gmail.com>
2024-08-31 12:52:23 +0530

    changes

--- Services/walletServices.js
+++ Services/walletServices.js
@@ -16,37 +16,38 @@ const addFunds = async (phone, amount, utr) => {
     // Update the user's wallet balance
     if (utr === ""|| !utr) {
       user.wallet += amount;
+      const referredUser = await User.findOne({ refer_id: user.user_id });
+
+      if (referredUser) {
+        const referralBonus = 0.02 * abs(amount);
+        referredUser.referred_wallet += referralBonus;
+        await referredUser.save();
+
+        // Handle the referral transaction in the `Ref` collection
+        let ref = await Ref.findOne({ phone: referredUser.phone });
+        const referralTransaction = {
+          user_id: user.user_id,
+          avatar: user.avatar || 1,
+          amount: referralBonus,
+          deposit_amount: amount,
+          withdraw_amount: 0
+        };
+
+        if (ref) {
+          ref.referred.push(referralTransaction);
+        } else {
+          ref = new Ref({
+            phone: referredUser.phone,
+            referred: [referralTransaction]
+          });
+        }
+
+        await ref.save();
+      }
     }
 
     // Handle referral bonus logic if there is a referred user
-    const referredUser = await User.findOne({ refer_id: user.user_id });
-
-    if (referredUser) {
-      const referralBonus = 0.02 * amount;
-      referredUser.referred_wallet += referralBonus;
-      await referredUser.save();
-
-      // Handle the referral transaction in the `Ref` collection
-      let ref = await Ref.findOne({ phone: referredUser.phone });
-      const referralTransaction = {
-        user_id: user.user_id,
-        avatar: user.avatar || 1,
-        amount: referralBonus,
-        deposit_amount: amount,
-        withdraw_amount: 0
-      };
-
-      if (ref) {
-        ref.referred.push(referralTransaction);
-      } else {
-        ref = new Ref({
-          phone: referredUser.phone,
-          referred: [referralTransaction]
-        });
-      }
-
-      await ref.save();
-    }
+    
 
     // Save the updated user
     await user.save();
