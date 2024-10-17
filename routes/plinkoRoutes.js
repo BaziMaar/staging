8e30a74663952abcb6357562f99d3fca9fadb983
Kanishk Pareek <knshkpareek@gmail.com>
2024-10-17 10:28:04 +0530

    changes

--- routes/plinkoRoutes.js
+++ routes/plinkoRoutes.js
@@ -1,25 +1,8 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
-const bodyParser = require('body-parser');
+const router = express.Router();
+const { generateController } = require('../controllers/plinkoController');
 
-const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
-const { generateController, savePlinkoEntry } = require('../controllers/plinkoController');
-
-// Middleware
-app.use(bodyParser.json()); // to parse JSON request body
-
-// Initialize socket.io
-generateController(io);
-
-// Define your routes
-app.post('/savePlinkoEntry', savePlinkoEntry(io)); // Post route for saving Plinko data
-
-// Start the server
-const PORT = 3000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+module.exports = (io) => {
+    generateController(io);  // Initialize the controller with the io instance
+    return router;
+};
